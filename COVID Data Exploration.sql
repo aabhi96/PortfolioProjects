9d46a57f64e824955edb8ad2f722a7bff749ce73
Abhishek Ajay <138313915+aabhi96@users.noreply.github.com>
2023-08-01 19:25:09 -0400

    COVID Data Exploration.sql

--- COVID Data Exploration.sql
+++ COVID Data Exploration.sql
@@ -3,17 +3,55 @@ from PortfolioProject..CovidDeaths
 Where continent is not null
 order by 3,4
 
---Select *
---from PortfolioProject..CovidVaccinations$
---order by 3,4
 
--- Select Data that we are going to be using
+
+--Global Numbers
+
+Select  SUM(new_cases) as Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null
+Order by 1,2
+
+-- Infection Per Continent
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+-- Showing Countries with the highest death count per population
+Select Location, MAX(CAST(Total_deaths as int) ) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by Location
+Order by TotalDeathCount desc
+
+--Looking at countries with the highest infection rate compared to population
+Select Location, Population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/Population))*100 AS PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by Location, Population
+Order by PercentPopulationInfected desc
+
+-- Shows the likelihood of dying if you contract covid in the United States
+Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
+From PortfolioProject..CovidDeaths
+Where location like '%states%'
+Order by 1,2
+
+
+
+
+
+-- Select Data that to be use
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
 Order by 1,2
 
 -- Looking at the total cases vs total deaths
--- Shows the likelihood of dying if you contract covid in United States
+-- Shows the likelihood of dying if you contract covid in the United States
 
 Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
 From PortfolioProject..CovidDeaths
@@ -21,7 +59,7 @@ Where location like '%states%'
 Order by 1,2
 
 -- Looking at Total Cases vs Population
--- Shows what percentage of population got covid
+-- Shows what percentage of the population got covid
 
 Select Location, date, total_cases,Population, (total_cases/Population)*100 AS PercentPopulationInfected
 From PortfolioProject..CovidDeaths
@@ -29,13 +67,6 @@ Where location like '%states%'
 and continent is not null
 Order by 1,2
 
---Looking at countries with highest infection rate compared to population
-
-Select Location, Population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/Population))*100 AS PercentPopulationInfected
-From PortfolioProject..CovidDeaths
-Where continent is not null
-Group by Location, Population
-Order by PercentPopulationInfected desc
 
 -- Showing Countries with the highest death count per population
 
@@ -45,21 +76,6 @@ Where continent is not null
 Group by Location
 Order by TotalDeathCount desc
 
-
--- Showing continents with the highest death count per population
-
-Select continent, MAX(CAST(Total_deaths as int) ) as TotalDeathCount
-From PortfolioProject..CovidDeaths
-Where continent is not null
-Group by continent
-Order by TotalDeathCount desc
-
---Global Numbers
-
-Select  SUM(new_cases) as Total_cases, SUM(cast(new_deaths as int)) as Total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 AS DeathPercentage
-From PortfolioProject..CovidDeaths
-where continent is not null
-Order by 1,2
 
 --Looking at Total population vs Vaccination
 
@@ -113,7 +129,6 @@ Select *, (RollingPeopleVaccinated/Population)*100 as RollingPercentVaccinated
 From #PercentPopulationVaccincated
 
 
--- if you have plan on making alteration to temp table
 
 DROP Table if exists #PercentPopulationVaccincated
 Create Table #PercentPopulationVaccincated
